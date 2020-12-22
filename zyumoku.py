c6cb2c80a8f0e1dcc246bba25e4c167f36571ee8
Ohnishi <s2022069@stu.musashino-u.ac.jp>
2020-12-22 17:30:52 +0900

    引数の追加 結局引数を減らすのは無理では？ 2セット目までは動作

--- zyumoku.py
+++ zyumoku.py
@@ -6,30 +6,32 @@ forward(-200)
 pendown()
 right(30)
 
-def zyumoku(num,count,ri_count):
+def zyumoku(num,count,ri_count,husa):
     if count == 5:
         if ri_count == 0:
             num = back(num)
             right(90)
-            zyumoku(num,count-1,ri_count+1)
+            zyumoku(num,count-1,ri_count+1,husa)
         elif ri_count == 1:
-            num = back(num)
-            left(30)
+            for i in range(husa):
+                num = back(num)
+                left(30)
             num = back(num)
             right(90)
-            zyumoku(num,count-2,ri_count-1)
+            husa +=1
+            zyumoku(num,count-2,ri_count - 1,husa)
 
     else:
         left(30)
         forward(num)
-        zyumoku(num * 2 / 3,count + 1, ri_count)
+        zyumoku(num * 2 / 3,count + 1, ri_count,husa)
 
 def back(a):
     a*=3/2
     forward(-a)
     return a
 
-zyumoku(f,0,0)
+zyumoku(f,0,0,1)
 
 mainloop()
 done()
