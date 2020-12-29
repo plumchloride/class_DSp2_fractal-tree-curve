4899c152490a2c0894b9f807886eb6c621587e04
Ohnishi <s2022069@stu.musashino-u.ac.jp>
2020-12-29 23:35:58 +0900

    調整

--- 2022069_zyumoku.py
+++ 2022069_zyumoku.py
@@ -1,3 +1,4 @@
+#gitリポジトリURL(https://github.com/plumchloride/class_DSp2_fractal-tree-curve)
 from turtle import *
 
 
@@ -26,44 +27,44 @@ done()
 
 # 再帰関数のことがよくわからなかったが、とりあえず試行錯誤した物
 # さすがにこれは再帰関数とは呼べないのでは？ 
-def zyumoku(num,count,ri_count,husa):
-    if count == 5:
-        if ri_count == 0:
-            num = back(num)
-            right(90)
-            zyumoku(num,count-1,ri_count+1,husa)
-        elif ri_count == 1:
-            if husa == 3:
-                for i in range(1):
-                    num = back(num)
-                    left(30)
-                num = back(num)
-                right(90)
-                zyumoku(num,count-2,ri_count - 1,husa+1)
-            elif husa == 4:
-                for i in range(3):
-                    num = back(num)
-                    left(30)
-                num = back(num)
-                right(90)
-                zyumoku(num,count-4,ri_count - 1,1)
-            for i in range(husa):
-                num = back(num)
-                left(30)
-            num = back(num)
-            right(90)
-            husa += 1
-            zyumoku(num,count-husa,ri_count - 1,husa)
+# def zyumoku(num,count,ri_count,husa):
+#     if count == 5:
+#         if ri_count == 0:
+#             num = back(num)
+#             right(90)
+#             zyumoku(num,count-1,ri_count+1,husa)
+#         elif ri_count == 1:
+#             if husa == 3:
+#                 for i in range(1):
+#                     num = back(num)
+#                     left(30)
+#                 num = back(num)
+#                 right(90)
+#                 zyumoku(num,count-2,ri_count - 1,husa+1)
+#             elif husa == 4:
+#                 for i in range(3):
+#                     num = back(num)
+#                     left(30)
+#                 num = back(num)
+#                 right(90)
+#                 zyumoku(num,count-4,ri_count - 1,1)
+#             for i in range(husa):
+#                 num = back(num)
+#                 left(30)
+#             num = back(num)
+#             right(90)
+#             husa += 1
+#             zyumoku(num,count-husa,ri_count - 1,husa)
 
-    else:
-        left(30)
-        forward(num)
-        zyumoku(num * 2 / 3,count + 1, ri_count,husa)
+#     else:
+#         left(30)
+#         forward(num)
+#         zyumoku(num * 2 / 3,count + 1, ri_count,husa)
 
-def back(a):
-    a*=3/2
-    forward(-a)
-    return a
+# def back(a):
+#     a*=3/2
+#     forward(-a)
+#     return a
 
 #left(90)
 # penup()
@@ -74,5 +75,3 @@ def back(a):
 
 # mainloop()
 # done()
-
-#一応gitで管理して作成していたリポジトリのURL(https://github.com/plumchloride/class_DSp2_fractal-tree-curve)
